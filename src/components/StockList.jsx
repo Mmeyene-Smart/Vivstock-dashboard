334cff64b7313be7e37663c8d596462b5e07a78f
Mmeyene Aloysius <148377305+Mmeyene-Smart@users.noreply.github.com>
2025-01-17 14:24:27 +0100

    Update StockList.jsx

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -59,10 +59,10 @@ function StockList() {
           {filteredStocks.map((stock) => (
             <div
               key={stock.symbol}
-              className="flex items-center justify-between cursor-pointer hover:bg-[#1E1E1E] p-4 rounded-lg transition-colors"
+              className="flex items-center justify-between cursor-pointer hover:bg-[#1E1E1E] p-4 rounded-lg transition-colors border-b border-gray-400"
               onClick={() => handleStockClick(stock.symbol)}
             >
-              <div className="flex items-center gap-3 w-full h-[50px] text-[15px] border-b border-gray-400">
+              <div className="flex items-center gap-3 w-full h-[60px] text-[15px]">
                 <img
                   src={stock.logo}
                   alt={stock.name}
