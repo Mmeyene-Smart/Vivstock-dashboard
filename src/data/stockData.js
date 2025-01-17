378a4417e7cd90b713e80cfa3090641449a0a994
Mmeyene Aloysius <148377305+Mmeyene-Smart@users.noreply.github.com>
2025-01-17 12:11:50 +0100

    Update stockData.js

--- src/data/stockData.js
+++ src/data/stockData.js
@@ -3,7 +3,7 @@ export const stockData = [
     name: "Access Holdings",
     symbol: "ACCESSCORP",
     price: "24.11",
-    logo: "https://example.com/access.png",
+    logo: "https://www.accessbankplc.com/Content/images/access-logo-2.png",
     isUSD: false
   },
   {
@@ -17,28 +17,28 @@ export const stockData = [
     name: "Apple Inc",
     symbol: "AAPL",
     price: "182.31",
-    logo: "https://example.com/apple.png",
+    logo: "https://www.flaticon.com/free-icon/apple-logo_747",
     isUSD: true
   },
   {
     name: "Microsoft",
     symbol: "MSFT",
     price: "403.78",
-    logo: "https://example.com/microsoft.png",
+    logo: "	https://cdn-dynmedia-1.microsoft.com/is/image/micr…/UHFbanner-MSlogo?fmt=png-alpha&bfc=off&qlt=100,1",
     isUSD: true
   },
   {
     name: "BUA Cement",
     symbol: "BUACEMENT",
     price: "93.50",
-    logo: "https://example.com/bua-cement.png",
+    logo: "	https://www.buacement.com/images/logo.png",
     isUSD: false
   },
   {
     name: "Tesla, Inc",
     symbol: "TSLA",
     price: "193.57",
-    logo: "https://example.com/tesla.png",
+    logo: "https://images.app.goo.gl/eg3rMQmrAJY8aDby8",
     isUSD: true
   }
 ];
