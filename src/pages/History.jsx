cd57c24a4086eaa0a2a2c080a84342d8d2cf2f6f
Mmeyene <mmeyenealoysius@gmail.com>
2025-01-20 18:13:18 +0100

    updated

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -3,9 +3,9 @@ import react from 'react';
 function History() {
     return (
         <div>
-        <h1 className='text-center text-4xl font-bold '>Transaction History</h1>
+        <h1 className='text-center text-[20px] font-bold '>Transaction History</h1>
 
-        <div className='text-center text-2xl font-bold font-[DMSans] opacity-15 absolute top-[55%] left-[20%]'>
+        <div className='text-center text-2xl font-bold font-[DMSans] opacity-15 absolute top-[45%] left-[20%]'>
             <p>
                 NO TRANSACTIONS 
             </p>
--- src/pages/Trade.jsx
+++ src/pages/Trade.jsx
@@ -3,9 +3,9 @@ import react from 'react';
 function Trade() {
     return (
         <div>
-        <h1 className='text-center text-4xl font-bold '>Trade</h1>
+        <h1 className='text-center text-[20px] font-bold '>Trade</h1>
 
-        <div className='text-center text-2xl font-bold font-[DMSans] opacity-15 absolute top-[55%] left-[50%]'>
+        <div className='text-center text-2xl font-bold font-[DMSans] opacity-15 absolute top-[45%] left-[20%]'>
             <p>
                 NO ACTIVE TRADES
             </p>
