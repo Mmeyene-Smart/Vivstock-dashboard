3f375f16e0bfe96b9f33b769fb6c5e4b4a8094c1
Mmeyene <mmeyenealoysius@gmail.com>
2025-01-20 18:19:22 +0100

    updated

--- src/pages/Trade.jsx
+++ src/pages/Trade.jsx
@@ -3,7 +3,7 @@ import react from 'react';
 function Trade() {
     return (
         <div>
-        <h1 className='text-center text-[20px] font-bold '>Trade</h1>
+        <h1 className='text-center text-[20px] font-semibold '>Trades</h1>
 
         <div className='text-center text-2xl font-bold font-[DMSans] opacity-15 absolute top-[45%] left-[20%]'>
             <p>
