a8ca4ee713deea31c6e4857ff49ff8cd0e4123a4
Mmeyene <mmeyenealoysius@gmail.com>
2025-01-20 18:22:09 +0100

    updated

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -3,7 +3,7 @@ import react from 'react';
 function History() {
     return (
         <div>
-        <h1 className='text-center text-[17px] font-semibold opacity-[0.8]'>Transaction History</h1>
+        <h1 className='text-center text-[18px] font-semibold opacity-[0.8]'>Transaction History</h1>
 
         <div className='text-center text-2xl font-bold font-[DMSans] opacity-15 absolute top-[45%] left-[20%]'>
             <p>
